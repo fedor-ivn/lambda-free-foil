19c70bebb099e6cc5d1ca060354f025254af3f6f
Nikolai Kudasov <nickolay.kudasov@gmail.com>
2024-08-06 12:50:44 +0300

    Add Show and IsString instances for Term n

--- src/Language/Lambda/Impl.hs
+++ src/Language/Lambda/Impl.hs
@@ -1,10 +1,12 @@
-{-# LANGUAGE DataKinds #-}
+{-# OPTIONS_GHC -ddump-splices #-}
+{-# LANGUAGE DataKinds         #-}
 {-# LANGUAGE DeriveTraversable #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE KindSignatures #-}
-{-# LANGUAGE LambdaCase #-}
-{-# LANGUAGE PatternSynonyms #-}
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE KindSignatures    #-}
+{-# LANGUAGE LambdaCase        #-}
+{-# LANGUAGE PatternSynonyms   #-}
+{-# LANGUAGE RankNTypes        #-}
+{-# LANGUAGE TemplateHaskell   #-}
 
 -- | Free foil implementation of the \(\lambda)-calculus (with pairs).
 --
@@ -32,19 +34,20 @@
 -- wildcard patterns and variable patterns are handled in this implementation.
 module Language.Lambda.Impl where
 
-import qualified Control.Monad.Foil as Foil
-import Control.Monad.Free.Foil
-import Control.Monad.Free.Foil.TH
-import Data.Bifunctor.TH
-import Data.Map (Map)
-import qualified Data.Map as Map
-import Data.String (IsString (..))
-import qualified Language.Lambda.Syntax.Abs as Raw
+import           Control.Monad.Foil            (Distinct)
+import qualified Control.Monad.Foil            as Foil
+import           Control.Monad.Free.Foil
+import           Control.Monad.Free.Foil.TH
+import           Data.Bifunctor.TH
+import           Data.Map                      (Map)
+import qualified Data.Map                      as Map
+import           Data.String                   (IsString (..))
+import qualified Language.Lambda.Syntax.Abs    as Raw
 import qualified Language.Lambda.Syntax.Layout as Raw
-import qualified Language.Lambda.Syntax.Lex as Raw
-import qualified Language.Lambda.Syntax.Par as Raw
-import qualified Language.Lambda.Syntax.Print as Raw
-import System.Exit (exitFailure)
+import qualified Language.Lambda.Syntax.Lex    as Raw
+import qualified Language.Lambda.Syntax.Par    as Raw
+import qualified Language.Lambda.Syntax.Print  as Raw
+import           System.Exit                   (exitFailure)
 
 -- $setup
 -- >>> :set -XOverloadedStrings
@@ -58,6 +61,10 @@ import System.Exit (exitFailure)
 -- ** Signature
 
 mkSignature ''Raw.Term ''Raw.VarIdent ''Raw.ScopedTerm ''Raw.Pattern
+-- data TermSig scope term
+--   = LamSig scope
+--   | AppSig term term
+
 deriveZipMatch ''TermSig
 deriveBifunctor ''TermSig
 deriveBifoldable ''TermSig
@@ -66,6 +73,21 @@ deriveBitraversable ''TermSig
 -- ** Pattern synonyms
 
 mkPatternSynonyms ''TermSig
+-- FV( (λ x. x) y )  =  { y }
+--
+-- λs. λz. s (s z)    :: Term VoidS
+--     λz. s (s z)    :: Term n1      --  n1 ~ { s }
+--         s (s z)    :: Term n2      --  n2 ~ { s, z }
+-- λs                 :: NameBinder VoidS n1
+--     λz             :: NameBinder n1 n2
+--
+-- pattern Lam :: Foil.NameBinder n l -> AST TermSig l -> AST TermSig n
+-- pattern Lam :: Foil.NameBinder n l -> Term l -> Term n
+-- pattern Lam b x = Node (LamSig (ScopedAST b x))
+--
+-- pattern App :: AST TermSig n -> AST TermSig n -> AST TermSig n
+-- pattern App :: Term n -> Term n -> Term n
+-- pattern App x0 x1 = Node (AppSig x0 x1)
 
 -- ** Conversion helpers
 
@@ -74,21 +96,56 @@ mkConvertFromFreeFoil ''Raw.Term ''Raw.VarIdent ''Raw.ScopedTerm ''Raw.Pattern
 
 -- * User-defined code
 
-type Term = AST
+-- | Scope-safe λ-term representation in scope @n@.
+type Term n = AST TermSig n
 
 -- ** Conversion helpers
 
 -- | Convert 'Raw.Term'' into a scope-safe term.
 -- This is a special case of 'convertToAST'.
-toTerm :: (Foil.Distinct n) => Foil.Scope n -> Map Raw.VarIdent (Foil.Name n) -> Raw.Term -> Term TermSig n
+toTerm :: (Foil.Distinct n) => Foil.Scope n -> Map Raw.VarIdent (Foil.Name n) -> Raw.Term -> Term n
 toTerm = convertToAST convertToTermSig getPatternBinder getTermFromScopedTerm
 
 -- | Convert 'Raw.Term'' into a closed scope-safe term.
 -- This is a special case of 'toTerm''.
-toTermClosed :: Raw.Term -> Term TermSig Foil.VoidS
+toTermClosed :: Raw.Term -> Term Foil.VoidS
 toTermClosed = toTerm Foil.emptyScope Map.empty
 
-whnf :: (Foil.Distinct n) => Foil.Scope n -> AST TermSig n -> AST TermSig n
+fromTerm :: Term n -> Raw.Term
+fromTerm = convertFromAST convertFromTermSig
+  Raw.Var
+  Raw.APattern
+  Raw.AScopedTerm
+  (\i -> Raw.VarIdent ("x" ++ show i))
+
+lam :: Distinct n => Foil.Scope n -> (forall l. (Foil.DExt n l) => Foil.Name l -> Term l) -> Term n
+lam scope makeBody = Foil.withFresh scope $ \x' ->
+  let x = Foil.nameOf x'
+   in Lam x' (makeBody x)
+
+-- >>> lam Foil.emptyScope (\x -> App (Var x) (Var x))
+-- λ x0 . x0 x0
+instance Show (Term n) where
+  show = Raw.printTree . fromTerm
+
+-- >>> "λy.(λx.λy.x)y" :: Term Foil.VoidS
+-- λ x0 . (λ x1 . λ x2 . x1) x0
+instance IsString (Term Foil.VoidS) where
+  fromString = unsafeParseTerm
+
+unsafeParseTerm :: String -> Term Foil.VoidS
+unsafeParseTerm input =
+  case Raw.pTerm tokens of
+    Left err   -> error err
+    Right term -> toTermClosed term
+  where
+    tokens = Raw.resolveLayout False (Raw.myLexer input)
+
+-- >>> whnf Foil.emptyScope (lam Foil.emptyScope (\x -> App (Var x) (Var x)))
+-- λ x0 . x0 x0
+-- >>> whnf Foil.emptyScope "(λs.λz.s (s z)) (λs.λz.s (s z))"
+-- λ x1 . (λ x0 . λ x1 . x0 (x0 x1)) ((λ x0 . λ x1 . x0 (x0 x1)) x1)
+whnf :: (Foil.Distinct n) => Foil.Scope n -> Term n -> Term n
 whnf scope = \case
   App f x ->
     case whnf scope f of
@@ -97,7 +154,6 @@ whnf scope = \case
          in whnf scope (substitute scope subst body)
       f' -> App f' x
   t -> t
-
 
 main :: IO ()
 main = do
