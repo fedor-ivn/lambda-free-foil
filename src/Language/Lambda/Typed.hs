8ee33bf645d22105d0ce627ebfee0180fcc739d1
Artem Starikov <contact@snejugal.ru>
2024-10-13 20:22:02 +0300

    Define metavariable and remove `isConstant` parameter

--- src/Language/Lambda/Typed.hs
+++ src/Language/Lambda/Typed.hs
@@ -1,7 +1,13 @@
+{-# LANGUAGE PatternSynonyms #-}
+
 module Language.Lambda.Typed (
   Type (..),
   Variable (..),
+  Metavariable (..),
+  Atom (..),
   Expression (..),
+  pattern Var,
+  pattern Metavar,
   typeOf,
   Heading (..),
   isRigid,
@@ -19,16 +25,30 @@ data Type
 
 newtype Variable = Variable String deriving (Eq, Show)
 
+newtype Metavariable = Metavariable String deriving (Eq, Show)
+
+data Atom
+  = AVar Variable
+  | AMetavar Metavariable
+  deriving (Eq, Show)
+
 data Expression
-  = Var Variable
+  = Atom Atom
   | Application Expression Expression
   | Lambda Variable Type Expression
   deriving (Eq, Show)
+
+pattern Var :: Variable -> Expression
+pattern Var variable = Atom (AVar variable)
+
+pattern Metavar :: Metavariable -> Expression
+pattern Metavar variable = Atom (AMetavar variable)
 
 -- >>> t = Base "t"
 -- >>> u = Base "u"
 -- >>> x = Variable "x"
 -- >>> y = Variable "y"
+--
 -- >>> typeOf (const (Just t)) (Var x)
 -- Just (Base "t")
 -- >>> typeOf (const Nothing) (Lambda x t (Var x))
@@ -39,73 +59,85 @@ data Expression
 -- Just (Base "t")
 -- >>> typeOf (const (Just u)) (Application (Lambda x t (Var x)) (Var x))
 -- Nothing
-typeOf :: (Variable -> Maybe Type) -> Expression -> Maybe Type
-typeOf typeOfVariable (Var variable) = typeOfVariable variable
-typeOf typeOfVariable (Application function argument) = do
-  functionType <- typeOf typeOfVariable function
-  argumentType <- typeOf typeOfVariable argument
+typeOf :: (Atom -> Maybe Type) -> Expression -> Maybe Type
+typeOf typeOfAtom (Atom atom) = typeOfAtom atom
+typeOf typeOfAtom (Application function argument) = do
+  functionType <- typeOf typeOfAtom function
+  argumentType <- typeOf typeOfAtom argument
   case functionType of
     Function argumentType' returnType
       | argumentType == argumentType' -> Just returnType
     _ -> Nothing
-typeOf typeOfVariable (Lambda boundVariable typ body) = do
+typeOf typeOfAtom (Lambda boundVariable typ body) = do
   returnType <- flip typeOf body $ \variable ->
-    if variable == boundVariable
+    if variable == AVar boundVariable
       then Just typ
-      else typeOfVariable variable
+      else typeOfAtom variable
   Just (Function typ returnType)
 
 data Heading = Heading
   { binder :: [(Variable, Type)]
-  , head :: Variable
+  , head :: Atom
   }
   deriving (Eq, Show)
 
--- >>> _C = Variable "C"
 -- >>> x = Variable "x"
 -- >>> y = Variable "y"
+-- >>> m = Metavariable "M"
 -- >>> t = Base "t"
 --
--- >>> isRigid (== _C) (Heading [(x, t)] x)
+-- >>> isRigid (Heading [(x, t)] (AVar x))
 -- True
--- >>> isRigid (== _C) (Heading [(x, t)] _C)
+-- >>> isRigid (Heading [(x, t)] (AVar y))
 -- True
--- >>> isRigid (== _C) (Heading [(x, t)] y)
+-- >>> isRigid (Heading [(x, t)] (AMetavar M))
 -- False
-isRigid :: (Variable -> Bool) -> Heading -> Bool
-isRigid isConstant (Heading binder' head') =
-  head' `elem` (fst <$> binder') || isConstant head'
+isRigid :: Heading -> Bool
+isRigid (Heading _ (AVar _)) = True
+isRigid (Heading _ (AMetavar _)) = False
 
 -- Ignores types of parameters (at least for now).
 --
+-- >>> a = Variable "a"
 -- >>> x = Variable "x"
 -- >>> y = Variable "y"
--- >>> a = Variable "a"
--- >>> b = Variable "b"
--- >>> q = Variable "q"
+-- >>> m = Metavariable "M"
+-- >>> n = Metavariable "N"
 -- >>> t = Base "t"
 --
--- >>> areAlphaEquivalent (Heading [(x, t), (y, t)] x) (Heading [(a, t), (b, t)] a)
+-- >>> areAlphaEquivalent (Heading [(x, t)] (AVar x)) (Heading [(y, t)] (AVar y))
 -- True
--- >>> areAlphaEquivalent (Heading [(x, t), (y, t)] x) (Heading [(b, t), (a, t)] a)
--- False
--- >>> areAlphaEquivalent (Heading [(x, t), (y, t)] x) (Heading [(b, t), (a, t)] x)
--- False
--- >>> areAlphaEquivalent (Heading [(x, t), (y, t)] a) (Heading [(b, t), (a, t)] a)
--- False
--- >>> areAlphaEquivalent (Heading [(x, t), (y, t)] q) (Heading [(b, t), (a, t)] q)
+-- >>> areAlphaEquivalent (Heading [(x, t)] (AVar a)) (Heading [(y, t)] (AVar a))
 -- True
+-- >>> areAlphaEquivalent (Heading [(x, t)] (AVar x)) (Heading [(y, t)] (AVar x))
+-- False
+-- >>> areAlphaEquivalent (Heading [(y, t)] (AVar x)) (Heading [(x, t)] (AVar x))
+-- False
+-- >>> areAlphaEquivalent (Heading [(x, t), (y, t)] (AVar x)) (Heading [(x, t)] (AVar x))
+-- False
+-- >>> areAlphaEquivalent (Heading [(x, t)] (AMetavar m)) (Heading [(y, t)] (AMetavar m))
+-- True
+-- >>> areAlphaEquivalent (Heading [(x, t)] (AMetavar m)) (Heading [(y, t), (x, t)] (AMetavar m))
+-- False
+-- >>> areAlphaEquivalent (Heading [(x, t)] (AMetavar n)) (Heading [(y, t)] (AMetavar m))
+-- False
 areAlphaEquivalent :: Heading -> Heading -> Bool
-areAlphaEquivalent (Heading [] leftHead) (Heading [] rightHead) =
+areAlphaEquivalent
+  (Heading leftBinder (AMetavar leftHead))
+  (Heading rightBinder (AMetavar rightHead)) =
+    leftHead == rightHead && length leftBinder == length rightBinder
+areAlphaEquivalent (Heading [] (AVar leftHead)) (Heading [] (AVar rightHead)) =
   leftHead == rightHead
 areAlphaEquivalent
-  (Heading ((leftVariable, _) : leftBinder) leftHead)
-  (Heading ((rightVariable, _) : rightBinder) rightHead)
+  (Heading ((leftVariable, _) : leftBinder) (AVar leftHead))
+  (Heading ((rightVariable, _) : rightBinder) (AVar rightHead))
     | leftVariable == leftHead =
         rightVariable == rightHead && length leftBinder == length rightBinder
     | rightVariable == rightHead = False
     | otherwise =
-        areAlphaEquivalent (Heading leftBinder leftHead) (Heading rightBinder rightHead)
+        areAlphaEquivalent
+          (Heading leftBinder (AVar leftHead))
+          (Heading rightBinder (AVar rightHead))
 areAlphaEquivalent _ _ = False
 
 data NormalTerm = NormalTerm
@@ -121,17 +153,17 @@ data NormalTerm = NormalTerm
 -- >>> u = Base "u"
 --
 -- >>> asNormalTerm (Var x)
--- Just (NormalTerm {heading = Heading {binder = [], head = Variable "x"}, arguments = []})
+-- Just (NormalTerm {heading = Heading {binder = [], head = AVar (Variable "x")}, arguments = []})
 -- >>> asNormalTerm (Application (Var x) (Var y))
--- Just (NormalTerm {heading = Heading {binder = [], head = Variable "x"}, arguments = [NormalTerm {heading = Heading {binder = [], head = Variable "y"}, arguments = []}]})
+-- Just (NormalTerm {heading = Heading {binder = [], head = AVar (Variable "x")}, arguments = [NormalTerm {heading = Heading {binder = [], head = AVar (Variable "y")}, arguments = []}]})
 -- >>> asNormalTerm (Application (Application (Var x) (Var y)) (Var z))
--- Just (NormalTerm {heading = Heading {binder = [], head = Variable "x"}, arguments = [NormalTerm {heading = Heading {binder = [], head = Variable "y"}, arguments = []},NormalTerm {heading = Heading {binder = [], head = Variable "z"}, arguments = []}]})
+-- Just (NormalTerm {heading = Heading {binder = [], head = AVar (Variable "x")}, arguments = [NormalTerm {heading = Heading {binder = [], head = AVar (Variable "y")}, arguments = []},NormalTerm {heading = Heading {binder = [], head = AVar (Variable "z")}, arguments = []}]})
 -- >>> asNormalTerm (Lambda z t (Var x))
--- Just (NormalTerm {heading = Heading {binder = [(Variable "z",Base "t")], head = Variable "x"}, arguments = []})
+-- Just (NormalTerm {heading = Heading {binder = [(Variable "z",Base "t")], head = AVar (Variable "x")}, arguments = []})
 -- >>> asNormalTerm (Lambda y t (Lambda z t (Var x)))
--- Just (NormalTerm {heading = Heading {binder = [(Variable "y",Base "t"),(Variable "z",Base "t")], head = Variable "x"}, arguments = []})
+-- Just (NormalTerm {heading = Heading {binder = [(Variable "y",Base "t"),(Variable "z",Base "t")], head = AVar (Variable "x")}, arguments = []})
 -- >>> asNormalTerm (Lambda z t (Application (Var x) (Var y)))
--- Just (NormalTerm {heading = Heading {binder = [(Variable "z",Base "t")], head = Variable "x"}, arguments = [NormalTerm {heading = Heading {binder = [], head = Variable "y"}, arguments = []}]})
+-- Just (NormalTerm {heading = Heading {binder = [(Variable "z",Base "t")], head = AVar (Variable "x")}, arguments = [NormalTerm {heading = Heading {binder = [], head = AVar (Variable "y")}, arguments = []}]})
 -- >>> asNormalTerm (Application (Lambda x t (Var x)) (Var y))
 -- Nothing
 -- >>> asNormalTerm (Lambda x t (Application (Lambda z t (Var z)) (Var y)))
@@ -147,7 +179,7 @@ asNormalTerm other = extractBody other
     NormalTerm heading' arguments' <- extractBody function
     argument' <- asNormalTerm argument
     Just (NormalTerm heading' (arguments' <> [argument']))
-  extractBody (Var head') = Just (NormalTerm (Heading [] head') [])
+  extractBody (Atom head') = Just (NormalTerm (Heading [] head') [])
 
 scopedArguments :: NormalTerm -> [NormalTerm]
 scopedArguments (NormalTerm (Heading scope _) arguments') =
@@ -171,43 +203,43 @@ data Node
 -- >>> a = Variable "a"
 -- >>> b = Variable "b"
 -- >>> c = Variable "c"
--- >>> f = Variable "f"
+-- >>> f = Metavariable "F"
 -- >>> u = Variable "u"
 -- >>> v = Variable "v"
 -- >>> w = Variable "w"
--- >>> x = Variable "x"
--- >>> y = Variable "y"
+-- >>> x = Metavariable "X"
+-- >>> y = Metavariable "Y"
 -- >>> t = Base "t"
--- >>> isConstant = (`elem` [a, b, c])
--- >>> var v = NormalTerm (Heading [] v) []
+-- >>> var v = NormalTerm (Heading [] (AVar v)) []
+-- >>> meta v = NormalTerm (Heading [] (AMetavar v)) []
 --
--- >>> left = NormalTerm (Heading [] a) [NormalTerm (Heading [(u, t)] b) [var x, var u], var c]
--- >>> right = NormalTerm (Heading [] a) [NormalTerm (Heading [(v, t)] b) [var y, var v], NormalTerm (Heading [] f) [var c]]
--- >>> simplified = simplify isConstant (DisagreementSet [(left, right)])
--- >>> expected = Nonterminal (DisagreementSet [(NormalTerm (Heading [(u, t)] x) [], NormalTerm (Heading [(v, t)] y) []), (NormalTerm (Heading [] f) [var c], var c)])
+-- >>> left = NormalTerm (Heading [] (AVar a)) [NormalTerm (Heading [(u, t)] (AVar b)) [meta x, var u], var c]
+-- >>> right = NormalTerm (Heading [] (AVar a)) [NormalTerm (Heading [(v, t)] (AVar b)) [meta y, var v], NormalTerm (Heading [] (AMetavar f)) [var c]]
+-- >>> simplified = simplify (DisagreementSet [(left, right)])
+-- >>> expected = Nonterminal (DisagreementSet [(NormalTerm (Heading [(u, t)] (AMetavar x)) [], NormalTerm (Heading [(v, t)] (AMetavar y)) []), (NormalTerm (Heading [] (AMetavar f)) [var c], var c)])
 -- >>> simplified == expected
 -- True
 --
--- >>> left = NormalTerm (Heading [] a) [NormalTerm (Heading [(u, t)] b) [var x, var u]]
--- >>> right = NormalTerm (Heading [] a) [NormalTerm (Heading [(v, t)] b) [var y, var v]]
--- >>> simplified = simplify isConstant (DisagreementSet [(left, right)])
--- >>> expected = Nonterminal (DisagreementSet [(NormalTerm (Heading [(u, t)] x) [], NormalTerm (Heading [(v, t)] y) [])])
+-- >>> left = NormalTerm (Heading [] (AVar a)) [NormalTerm (Heading [(u, t)] (AVar b)) [meta x, var u]]
+-- >>> right = NormalTerm (Heading [] (AVar a)) [NormalTerm (Heading [(v, t)] (AVar b)) [meta y, var v]]
+-- >>> simplified = simplify (DisagreementSet [(left, right)])
+-- >>> expected = Nonterminal (DisagreementSet [(NormalTerm (Heading [(u, t)] (AMetavar x)) [], NormalTerm (Heading [(v, t)] (AMetavar y)) [])])
 -- >>> simplified == expected
 -- True
 --
--- >>> left = NormalTerm (Heading [(u, t), (v, t)] a) [var y, NormalTerm (Heading [(w, t)] v) []]
--- >>> right = NormalTerm (Heading [(v, t), (w, t)] a) [var v, NormalTerm (Heading [(u, t)] b) []]
--- >>> simplify isConstant (DisagreementSet [(left, right)])
+-- >>> left = NormalTerm (Heading [(u, t), (v, t)] (AVar a)) [var u, NormalTerm (Heading [(w, t)] (AVar v)) []]
+-- >>> right = NormalTerm (Heading [(v, t), (w, t)] (AVar a)) [var v, NormalTerm (Heading [(u, t)] (AVar v)) []]
+-- >>> simplify (DisagreementSet [(left, right)])
 -- Failure
-simplify :: (Variable -> Bool) -> DisagreementSet -> Node
-simplify isConstant (DisagreementSet set) = case simplifyRigidPairs set of
+simplify :: DisagreementSet -> Node
+simplify (DisagreementSet set) = case simplifyRigidPairs set of
   Nothing -> Failure
   Just set' -> Nonterminal (DisagreementSet (putRigidOnRight <$> set'))
  where
   simplifyRigidPairs = foldr simplifyRigidPair (Just [])
   simplifyRigidPair pair@(left, right) rest
-    | not (isRigid isConstant (heading left)) = (pair :) <$> rest
-    | not (isRigid isConstant (heading right)) = (pair :) <$> rest
+    | not (isRigid (heading left)) = (pair :) <$> rest
+    | not (isRigid (heading right)) = (pair :) <$> rest
     | not (areAlphaEquivalent (heading left) (heading right)) = Nothing
     | otherwise = do
         newPairs <- simplifyRigidPairs (zip (scopedArguments left) (scopedArguments right))
@@ -216,5 +248,5 @@ simplify isConstant (DisagreementSet set) = case simplifyRigidPairs set of
 
   -- TODO: is this really needed here?
   putRigidOnRight (left, right)
-    | isRigid isConstant (heading left) = (right, left)
+    | isRigid (heading left) = (right, left)
     | otherwise = (left, right)
