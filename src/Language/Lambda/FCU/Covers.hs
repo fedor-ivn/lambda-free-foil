0bdb1cd27bd6a1acb6e2f68fdfc64f277cb97750
Damir Afliatonov <afl_damir@mail.ru>
2024-10-23 01:05:05 +0000

    Add cover finding algorithm

---
+++ src/Language/Lambda/FCU/Covers.hs
@@ -0,0 +1,60 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+
+module Language.Lambda.FCU.Covers (findCover, coverExists, CoverTest (..), runCoverTest) where
+
+import Data.Maybe (isJust)
+import Language.Lambda.FCU.RTerms (RTerm (..), toRTerm, toTerm)
+import Language.Lambda.FCU.Terms (Term (..))
+
+findCover :: [RTerm] -> RTerm -> Maybe RTerm
+findCover params r =
+  case lookup r paramVars of
+    Just x -> Just x
+    Nothing -> case r of
+      RO {} -> Nothing
+      RConstructor {} -> Just r
+      RApp f b -> do
+        fCover <- findCover params f
+        bCover <- findCover params b
+        return (RApp fCover bCover)
+  where
+    boundVars = [RO ("x" ++ show i) | i <- [1 ..]]
+    paramVars = zip params boundVars
+
+coverExists :: [RTerm] -> RTerm -> Bool
+coverExists params r = isJust (findCover params r)
+
+-- Cons x y, [y, x]
+-- >>> findCover [RO "y", RO "x"] (RApp (RApp (RConstructor "Cons") (RO "x")) (RO "y"))
+-- Just (RApp (RApp (RConstructor "Cons") (RO "x2")) (RO "x1"))
+
+data CoverTest = CoverTest
+  { coverTestParams :: [Term],
+    coverTestRHS :: Term
+  }
+
+runCoverTest :: CoverTest -> Maybe Term
+runCoverTest CoverTest {..} = toTerm <$> findCover (map toRTerm coverTestParams) (toRTerm coverTestRHS)
+
+coverTest1 :: CoverTest
+coverTest1 =
+  CoverTest
+    { coverTestParams = ["y", "x"],
+      coverTestRHS = "Cons" :@ "x" :@ "y"
+    }
+
+-- >>> runCoverTest coverTest1
+-- Just ((Constructor "Cons" :@ O "x2") :@ O "x1")
+
+coverTest2 :: CoverTest
+coverTest2 =
+  CoverTest
+    { -- snd l, z, cons (fst x) y
+      coverTestParams = ["Snd" :@ "l", "z", "Cons" :@ ("Fst" :@ "x") :@ "y"],
+      -- cons (cons (cons (fst x) y) z) (snd l)
+      coverTestRHS = "Cons" :@ ("Cons" :@ ("Cons" :@ ("Fst" :@ "x") :@ "y") :@ "z") :@ ("Snd" :@ "l")
+    }
+
+-- >>> runCoverTest coverTest2
+-- Just ((Constructor "Cons" :@ ((Constructor "Cons" :@ O "x3") :@ O "x2")) :@ O "x1")
