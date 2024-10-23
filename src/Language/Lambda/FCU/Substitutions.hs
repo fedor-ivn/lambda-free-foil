344a901bab3b3662ff55a2357e99f1174f4535b1
Damir Afliatonov <afl_damir@mail.ru>
2024-10-23 01:05:25 +0000

    Add substitutions and their application

---
+++ src/Language/Lambda/FCU/Substitutions.hs
@@ -0,0 +1,29 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+
+module Language.Lambda.FCU.Substitutions where
+
+import Language.Lambda.FCU.Terms (Term (..))
+
+newtype Substitutions = Substitutions [(Term, Term)]
+
+-- theta S -> new S
+replaceTerm :: (Term, Term) -> Term -> Term
+replaceTerm (from, to) term = case term of
+  W x -> if W x == from then to else W x
+  O x -> if O x == from then to else O x
+  Constructor x -> Constructor x
+  f :@ x -> replaceTerm (from, to) f :@ replaceTerm (from, to) x
+  x :.: y -> x :.: replaceTerm (from, to) y
+
+applySubstitutions :: Substitutions -> Term -> Term
+applySubstitutions (Substitutions subs) term = foldr replaceTerm term subs
+
+-- >>> applySubstitutions (Substitutions [(O "x", W "y")]) (O "x")
+-- y
+
+-- >>> applySubstitutions (Substitutions [(O "x", W "y")]) (O "z")
+-- z
+
+-- >>> applySubstitutions (Substitutions [("X", "Y"), ("z", "fst" :@ "a")]) ("X" :@ "z")
+-- Y (fst (a))
