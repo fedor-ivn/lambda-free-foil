045ecc59e26a62d64627ab9a1cc5c5484bcc85dd
Damir Afliatonov <afl_damir@mail.ru>
2024-10-23 01:04:10 +0000

    Add updated terms implementation

---
+++ src/Language/Lambda/FCU/Terms.hs
@@ -0,0 +1,44 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Language.Lambda.FCU.Terms where
+
+import Data.Char (isUpper)
+import Data.String (IsString (..))
+
+-- $setup
+-- >>> :set -XOverloadedStrings
+
+type Id = String
+
+------- Metavars - Bound vars - Functions -- Application - Abstraction
+data Term = W Id | O Id | Constructor Id | Term :@ Term | Id :.: Term
+  deriving (Eq)
+
+instance IsString Term where
+  fromString s@(c : _)
+    | isUpper c && length s == 1 = W s
+    | isUpper c = Constructor s
+    | otherwise = O s
+  fromString _ = error "empty atoms are not allowed"
+
+instance Show Term where
+  show = ppTerm
+
+ppTerm :: Term -> String
+ppTerm (W x) = x
+ppTerm (O x) = x
+ppTerm (Constructor x) = x
+ppTerm (f :@ x) = ppTerm f ++ " (" ++ ppTerm x ++ ")"
+ppTerm (x :.: y) = "λ" ++ x ++ " . (" ++ ppTerm y ++ ")"
+
+-- >>> "x" :: Term
+-- x
+
+-- >>> "X" :: Term
+-- X
+
+-- >>> "Cons" :: Term
+-- Cons
+
+-- >>> "x" :.: ("Cons" :@ "x" :@ "y") :: Term
+-- λx . (Cons (x) (y))
